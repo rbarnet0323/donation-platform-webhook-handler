042708d1c41ddc2b7e3e28ff6a0fffd22ec7294b
rbarnet0323 <rbarnet6@gmail.com>
2022-08-19 23:12:51 -0700

    stripe

---
+++ webhook-handler.ts
@@ -0,0 +1,40 @@
+/** @jsx h */
+import Stripe from "https://esm.sh/stripe?target=deno";
+import { serve } from "https://deno.land/std@0.142.0/http/server.ts";
+import Handlebars from "https://dev.jspm.io/handlebars@4.7.6";
+
+const stripe = Stripe("sk_test_51LWRSgJCDjxWO9cJNB4j8iMgZd68TzXoPJ48IMjuZMrYKBFzEu5vMr7hvzV1kOyTxoTfDsSclaus35ymQUSjbIL600K38H6PJP", {
+  httpClient: Stripe.createFetchHttpClient(),
+});
+var session;
+
+const handler = async (req) => {
+  if (req.method === "POST") {
+    const url = new URL(req.url);
+    if (url.pathname == "/stripe") {
+      const res = await req.json();
+      const stripeID = res.id;
+      session = await stripe.checkout.sessions.retrieve(stripeID);
+      const body = {
+        data: {
+          amount: parseFloat((session.amount_total / 100).toFixed(2)),
+          user: session.metadata.userID,
+          donor: session.customer_details.email,
+          stripeID: stripeID,
+        },
+      }
+      await fetch("https://donation-platform-back-end.herokuapp.com/api/donations", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+    }
+  }
+  return new Response("200 OK", {
+    headers: { "content-type": "text/plain" },
+  });
+}
+
+serve(handler, { port: 8000 });
